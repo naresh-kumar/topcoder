f8976406014ff546bbe06d37c93fdee21b38c22b
Naresh <nareshkumargangwar@gmail.com>
2013-02-02 23:53:23 +0530

    codechef feb13 card game

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,85 @@
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.StringTokenizer;
 
 public class Main
 {
+    static int MOD = 1000000007;
+    static int ie = 1000000;
+    static long[] IE = new long[ie];
+    static long[] arr = new long[1001];
+
     public static void main(String[] args) throws IOException
     {
         Reader reader = new Reader(System.in);
         Writer writer = new Writer(System.out, false);
         //        Reader reader = new Reader("in.txt");
         //        Writer writer = new Writer(new FileOutputStream("output"), false);
-        feb13ClimbingStairs(reader, writer);
+        feb13CardGame(reader, writer);
 
         reader.close();
         writer.finish();
     }
 
-    private static void feb13ClimbingStairs(Reader reader, Writer writer)
+    private static void feb13CardGame(Reader reader, Writer writer) throws IOException
     {
+        arr[0] = 1;
+        arr[1] = 1;
+        for (int i = 2; i < 1001; i++)
+            arr[i] = (arr[i - 1] * i) % MOD;
 
+        Arrays.fill(IE, -1);
 
+        int t = reader.nextInt();
+        while (t-- > 0)
+        {
+            int n = reader.nextInt();
+            for (int i = 0; i < n; ++i) reader.nextInt();
+
+            long sol = 0;
+            for (int i = n / 2 + 1; i <= n; ++i)
+            {
+                sol = (sol + nCr(n, i)) % MOD;
+            }
+            writer.append(sol + "\n");
+        }
+    }
+
+    static long pow(int a, int b)
+    {
+        long x = 1, y = a;
+        while (b > 0)
+        {
+            if (b % 2 == 1)
+            {
+                x = (x * y);
+                if (x > MOD) x %= MOD;
+            }
+            y = (y * y);
+            if (y > MOD) y %= MOD;
+            b /= 2;
+        }
+        return x;
+    }
+
+    static long InverseEuler(int n)
+    {
+        if (n < ie)
+        {
+            if (IE[n] == -1)
+                IE[n] = pow(n, MOD - 2);
+            return IE[n];
+        }
+
+        return pow(n, MOD - 2);
+    }
+
+    static long nCr(int n, int r)
+    {
+        r = Math.min(r, n - r);
+        return (arr[n] * ((InverseEuler((int) arr[r]) * InverseEuler((int) arr[n - r])) % MOD)) % MOD;
     }
 
 
--- src/solutions.java
+++ src/solutions.java
@@ -3,6 +3,11 @@ import java.util.Arrays;
 
 public class solutions
 {
+    static int MOD = 1000000007;
+    static int ie = 1000000;
+    static long[] IE = new long[ie];
+    static long[] arr = new long[1001];
+
     private static void hackerCup3(Main.Reader reader, Main.Writer writer) throws IOException
     {
         int t = reader.nextInt();
@@ -103,5 +108,107 @@ public class solutions
             writer.append(cost + "\n");
         }
     }
+
+    private static void feb13ClimbingStairs(Main.Reader reader, Main.Writer writer) throws IOException
+    {
+        int MOD = 1000000007;
+        int t = reader.nextInt();
+        int[] sol = new int[1000001];
+        int max = 2;
+        sol[1] = 1;
+        sol[2] = 2;
+        while (t-- > 0)
+        {
+            int n = reader.nextInt();
+            int g = reader.nextInt();
+
+            if (n > max)
+            {
+                for (int i = max + 1; i <= n; ++i)
+                {
+                    long temp = sol[i - 1] + sol[i - 2];
+                    temp %= MOD;
+                    sol[i] = (int) temp;
+                }
+                max = n;
+            }
+            int count = countOne(sol[n]);
+            writer.append(count == g ? "CORRECT\n" : "INCORRECT\n");
+        }
+    }
+
+    private static int countOne(int i)
+    {
+        int count = 0;
+        while (i != 0)
+        {
+            if ((i & 1) == 1)
+            {
+                ++count;
+            }
+            i = i >> 1;
+        }
+        return count;
+    }
+
+    private static void feb13CardGame(Main.Reader reader, Main.Writer writer) throws IOException
+    {
+        arr[0] = 1;
+        arr[1] = 1;
+        for (int i = 2; i < 1001; i++)
+            arr[i] = (arr[i - 1] * i) % MOD;
+
+        Arrays.fill(IE, -1);
+
+        int t = reader.nextInt();
+        while (t-- > 0)
+        {
+            int n = reader.nextInt();
+            for (int i = 0; i < n; ++i) reader.nextInt();
+
+            long sol = 0;
+            for (int i = n / 2 + 1; i <= n; ++i)
+            {
+                sol = (sol + nCr(n, i)) % MOD;
+            }
+            writer.append(sol + "\n");
+        }
+    }
+
+    static long pow(int a, int b)
+    {
+        long x = 1, y = a;
+        while (b > 0)
+        {
+            if (b % 2 == 1)
+            {
+                x = (x * y);
+                if (x > MOD) x %= MOD;
+            }
+            y = (y * y);
+            if (y > MOD) y %= MOD;
+            b /= 2;
+        }
+        return x;
+    }
+
+    static long InverseEuler(int n)
+    {
+        if (n < ie)
+        {
+            if (IE[n] == -1)
+                IE[n] = pow(n, MOD - 2);
+            return IE[n];
+        }
+
+        return pow(n, MOD - 2);
+    }
+
+    static long nCr(int n, int r)
+    {
+        r = Math.min(r, n - r);
+        return (arr[n] * ((InverseEuler((int) arr[r]) * InverseEuler((int) arr[n - r])) % MOD)) % MOD;
+    }
+
 
 }
