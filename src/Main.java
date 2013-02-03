2897f2ede6ef536aba38d89e3aad1cebe1703348
Naresh <nareshkumargangwar@gmail.com>
2013-02-03 12:15:50 +0530

    solved dp in hacker cup round 1

--- src/Main.java
+++ src/Main.java
@@ -1,85 +1,112 @@
 
 import java.io.*;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.StringTokenizer;
 
 public class Main
 {
-    static int MOD = 1000000007;
-    static int ie = 1000000;
-    static long[] IE = new long[ie];
-    static long[] arr = new long[1001];
 
     public static void main(String[] args) throws IOException
     {
         Reader reader = new Reader(System.in);
         Writer writer = new Writer(System.out, false);
         //        Reader reader = new Reader("in.txt");
-        //        Writer writer = new Writer(new FileOutputStream("output"), false);
-        feb13CardGame(reader, writer);
+        //        Writer writer = new Writer(new FileOutputStream("output.txt"), false);
+        hackerCupDeadPixels(reader, writer);
 
         reader.close();
         writer.finish();
     }
 
-    private static void feb13CardGame(Reader reader, Writer writer) throws IOException
+    private static class DPPoint
     {
-        arr[0] = 1;
-        arr[1] = 1;
-        for (int i = 2; i < 1001; i++)
-            arr[i] = (arr[i - 1] * i) % MOD;
+        int left;
+        int up;
+    }
 
-        Arrays.fill(IE, -1);
-
+    private static void hackerCupDeadPixels(Reader reader, Writer writer) throws IOException
+    {
         int t = reader.nextInt();
+        int testCases = t;
         while (t-- > 0)
         {
-            int n = reader.nextInt();
-            for (int i = 0; i < n; ++i) reader.nextInt();
+            final int w = reader.nextInt();
+            final int h = reader.nextInt();
+            final int p = reader.nextInt();
+            final int q = reader.nextInt();
+            final int n = reader.nextInt();
+            final int x = reader.nextInt();
+            final int y = reader.nextInt();
+            final int a = reader.nextInt();
+            final int b = reader.nextInt();
+            final int c = reader.nextInt();
+            final int d = reader.nextInt();
 
-            long sol = 0;
-            for (int i = n / 2 + 1; i <= n; ++i)
+            int[][] monitor = new int[h][w];
+
+            int deadX = x;
+            int deadY = y;
+
+            monitor[deadY][deadX] = 1;
+            for (int i = 1; i < n; ++i)
             {
-                sol = (sol + nCr(n, i)) % MOD;
+                int tempDeadX = deadX;
+                deadX = (deadX * a + deadY * b + 1) % w;
+                deadY = (tempDeadX * c + deadY * d + 1) % h;
+                monitor[deadY][deadX] = 1;
             }
-            writer.append(sol + "\n");
-        }
-    }
 
-    static long pow(int a, int b)
-    {
-        long x = 1, y = a;
-        while (b > 0)
-        {
-            if (b % 2 == 1)
+//            for (int i = 0; i < h; ++i)
+//            {
+//                for (int j = 0; j < w; ++j)
+//                {
+//                    writer.append(monitor[i][j] + "");
+//                }
+//                writer.append("\n");
+//            }
+
+            DPPoint[][] dp = new DPPoint[h][w];
+
+            int ans = 0;
+
+            for (int i = 0; i < h; ++i)
             {
-                x = (x * y);
-                if (x > MOD) x %= MOD;
+                for (int j = 0; j < w; ++j)
+                {
+                    DPPoint dpPoint = new DPPoint();
+                    if (monitor[i][j] == 0)
+                    {
+                        dpPoint.up = i > 0 ? dp[i - 1][j].up + 1 : 1;
+                        dpPoint.left = j > 0 ? dp[i][j - 1].left + 1 : 1;
+                    }
+                    dp[i][j] = dpPoint;
+
+                    ans += check(dp, i, j, p, q);
+                }
             }
-            y = (y * y);
-            if (y > MOD) y %= MOD;
-            b /= 2;
+
+            writer.append("Case #" + (testCases - t) + ": " + ans + "\n");
         }
-        return x;
+
     }
 
-    static long InverseEuler(int n)
+    private static int check(DPPoint[][] dp, int i, int j, int p, int q)
     {
-        if (n < ie)
+        if (i >= q - 1 && j >= p - 1)
         {
-            if (IE[n] == -1)
-                IE[n] = pow(n, MOD - 2);
-            return IE[n];
+
+            int min = Math.min(p, q);
+            for (int loop = 0; loop < min; ++loop)
+            {
+                DPPoint dpPoint = dp[i - loop][j - loop];
+                if (dpPoint.up < q - loop || dpPoint.left < p - loop)
+                {
+                    return 0;
+                }
+            }
+            return 1;
         }
-
-        return pow(n, MOD - 2);
-    }
-
-    static long nCr(int n, int r)
-    {
-        r = Math.min(r, n - r);
-        return (arr[n] * ((InverseEuler((int) arr[r]) * InverseEuler((int) arr[n - r])) % MOD)) % MOD;
+        return 0;
     }
 
 
