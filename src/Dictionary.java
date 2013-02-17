82dcfc3fd815fb7a71997e0bec2a8844e7e69670
Naresh <nareshkumargangwar@gmail.com>
2013-02-17 12:38:58 +0530

    dict

---
+++ src/Dictionary.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dictionary
+{
+    Node head = new Node();
+
+    public void addWord(String word, String meaning)
+    {
+        Node node = head;
+        for (char c : word.toCharArray())
+        {
+            boolean exist = false;
+            for (Node n : node.children)
+            {
+                if (n.c == c)
+                {
+                    exist = true;
+                    node = n;
+                }
+            }
+
+            if (!exist)
+            {
+                Node node1 = new Node();
+                node1.c = c;
+                node.children.add(node1);
+                node = node1;
+            }
+        }
+        node.meaning = meaning;
+    }
+
+    public String meaning(String word)
+    {
+        Node node = head;
+        for (char c : word.toCharArray())
+        {
+            boolean exist = false;
+            for (Node n : node.children)
+            {
+                if (n.c == c)
+                {
+                    exist = true;
+                    node = n;
+                }
+            }
+
+            if (!exist)
+            {
+                return null;
+            }
+        }
+        return node.meaning;
+    }
+
+    private static class Node
+    {
+        char c;
+        List<Node> children = new ArrayList<Node>();
+        String meaning;
+    }
+}
