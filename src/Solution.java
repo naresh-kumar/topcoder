e41df800eca8561b9dc8f47395662fea9f8a905d
Naresh <nareshkumargangwar@gmail.com>
2013-02-17 18:16:13 +0530

    count numbers

--- src/Solution.java
+++ src/Solution.java
@@ -1,7 +1,6 @@
 
 import java.io.*;
-import java.util.Collection;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Solution
 {
@@ -9,12 +8,52 @@ public class Solution
 
     public static void main(String[] args) throws IOException
     {
-        Reader reader = new Reader("in.txt");
+        Reader reader = new Reader(System.in);
         Writer writer = new Writer(System.out);
+
+        int n = reader.nextInt();
+
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < n; ++i)
+        {
+            int x = reader.nextInt();
+            Integer count = map.get(x);
+            if (count == null)
+            {
+                count = 0;
+            }
+
+            count++;
+            map.put(x, count);
+        }
+
+        List<Integer> ans = new ArrayList<Integer>();
+
+        int maxCount = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet())
+        {
+            Integer number = entry.getKey();
+            Integer count = entry.getValue();
+            if (count > maxCount)
+            {
+                maxCount = count;
+                ans.clear();
+                ans.add(number);
+            }
+            else if (count == maxCount)
+            {
+                ans.add(number);
+            }
+        }
+
+        for (Integer integer : ans)
+        {
+            writer.append(integer + "\n");
+        }
 
         reader.close();
         writer.finish();
-
     }
 
     public static class Reader extends BufferedReader
