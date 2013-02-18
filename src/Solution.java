1423d157a7e60c5b696c1d42805f1b58730acc98
Naresh <nareshkumargangwar@gmail.com>
2013-02-18 20:07:45 +0530

    added test class

--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,7 @@
 
 import java.io.*;
-import java.util.*;
+import java.util.Collection;
+import java.util.StringTokenizer;
 
 public class Solution
 {
@@ -10,47 +11,6 @@ public class Solution
     {
         Reader reader = new Reader(System.in);
         Writer writer = new Writer(System.out);
-
-        int n = reader.nextInt();
-
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-
-        for (int i = 0; i < n; ++i)
-        {
-            int x = reader.nextInt();
-            Integer count = map.get(x);
-            if (count == null)
-            {
-                count = 0;
-            }
-
-            count++;
-            map.put(x, count);
-        }
-
-        List<Integer> ans = new ArrayList<Integer>();
-
-        int maxCount = 0;
-        for (Map.Entry<Integer, Integer> entry : map.entrySet())
-        {
-            Integer number = entry.getKey();
-            Integer count = entry.getValue();
-            if (count > maxCount)
-            {
-                maxCount = count;
-                ans.clear();
-                ans.add(number);
-            }
-            else if (count == maxCount)
-            {
-                ans.add(number);
-            }
-        }
-
-        for (Integer integer : ans)
-        {
-            writer.append(integer + "\n");
-        }
 
         reader.close();
         writer.finish();
---
+++ tests/Tests.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class Tests
+{
+    @Test
+    public void helloWorld()
+    {
+        System.out.println("hello world");
+    }
+}
