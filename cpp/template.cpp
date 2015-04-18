ff990064f58397a3c50f42eebcf7e1ae53c675e6
Naresh Kumar <knaresh@fb.com>
2015-04-18 00:11:33 -0700

    merge template and utils

--- cpp/template.cpp
+++ cpp/template.cpp
@@ -50,6 +50,32 @@ typedef map<S, S> MSS;
 
 #define INF 1000000000
 
+template<class T>
+T gcd(T a, T b) {
+    if (b == 0) return a;
+    else return gcd(b, a % b);
+}
+
+struct Point {
+  int x;
+  int y;
+}
+
+void printMap(T m) {
+    for(const auto& i : m) {
+      cout << i.first << " " << i.second << " ";
+    }
+    cout << endl;
+}
+
+template<typename T>
+void printList(T l) {
+    for(const auto& i : l) {
+      cout << i << " ";
+    }
+    cout << endl;
+}
+
 int main() {
     freopen("/Users/knaresh/codechef/codechef/codechef/in.txt", "r", stdin);
     freopen("/Users/knaresh/codechef/codechef/codechef/out.txt", "w", stdout);
