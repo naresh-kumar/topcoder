751699c92c85494926212126c4d4ecb956cd7efa
Naresh Kumar <knaresh@fb.com>
2015-06-07 02:30:37 -0700

    added codejam solutions

--- cpp/template.cpp
+++ cpp/template.cpp
@@ -21,74 +21,120 @@
 
 using namespace std;
 
-#define REP(i, n) for(int i=0; i<(n); i++)
-#define FOR(i, a, b) for(int i=(a); i<(b); i++)
-#define IFOR(i, a, b) for(int i=(a); i>=(b); i--)
-#define FORD(i, a, b, c) for(int i=(a); i<(b); i+=(c))
+#define si(x) ((int)x.size())
+#define pb(x) push_back(x)
+#define mp(a,b) make_pair(a, b)
+#define all(a) a.begin(),a.end()
+#define ss ({int x;scanf("%d", &x);x;})
+#define ssl ({long long int x;scanf("%lld", &x);x;})
+#define rep(i, a, b) for(int i=(a); i<(b); ++i)
+#define irep(i, a, b) for(int i=(a); i>=(b); --i)
+#define iter(i,v) for(auto i : (v))
 
-#define SI(x) ((int)x.size())
-#define PB(x) push_back(x)
-#define MP(a,b) make_pair(a, b)
-#define SORT(a) sort(a.begin(),a.end())
-#define ITER(it,a) for(typeof(a.begin()) it=a.begin();it!=a.end();it++)
-#define ALL(a) a.begin(),a.end()
-#define SS ({int x;scanf("%d", &x);x;})
-#define SSL ({long long int x;scanf("%lld", &x);x;})
-
-#define V vector
-#define S string
-#define FST first
-#define SEC second
-typedef V<int> VI;
-typedef V<S> VS;
-typedef long long LL;
-typedef pair<int, int> PII;
-typedef pair<S, int> PSI;
-typedef pair<S, S> PSS;
-typedef map<S, int> MSI;
-typedef map<S, S> MSS;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef pair<int, int> pii;
+typedef pair<string, int> psi;
+typedef pair<int, string> pis;
+typedef pair<string, string> pss;
+typedef map<string, int> msi;
+typedef map<string, string> mss;
+typedef map<int, int> mii;
+typedef map<int, string> mis;
 
 #define INF 1000000000
+#define PI  3.14159265358979323846  /* pi */
+
+template<class T> void setmax(T &a, T b) { if(b>a) a=b; }
+template<class T> void setmin(T &a, T b) { if(b<a) a=b; }
+template<class T> T gcd(T a, T b) { return b==0?a:gcd(b,a%b); }
+
+struct Point { int x; int y; };
 
 template<class T>
-T gcd(T a, T b) {
-    if (b == 0) return a;
-    else return gcd(b, a % b);
-}
-
-struct Point {
-  int x;
-  int y;
-};
-
-template<class T>
-void printMap(T m) {
-    for(const auto& i : m) {
-      cout << i.first << " " << i.second << " ";
-    }
-    cout << endl;
+void printmap(T m) {
+  for(const auto& i : m) cout << i.first << " " << i.second << " ";
+  cout << endl;
 }
 
 template<typename T>
-void printList(T l) {
-    for(const auto& i : l) {
-      cout << i << " ";
-    }
-    cout << endl;
+void printlist(T l) {
+  for(const auto& i : l) cout << i << " ";
+  cout << endl;
 }
 
 bool valid(int i, int j, int n) {
-    return i>=0 && i<n && j>=0 && j<n;
+  return i>=0 && i<n && j>=0 && j<n;
+}
+
+template<typename T>
+T reverse(T n) {
+  T reverse = 0;
+  while (n != 0) {
+    reverse = reverse * 10;
+    reverse = reverse + n%10;
+    n = n/10;
+  }
+  return reverse;
+}
+
+template<typename T>
+int firstdigit(T n) {
+  int ans = 0;
+  while (n != 0) {
+    ans = n%10;
+    n = n/10;
+  }
+  return ans;
+}
+
+template<typename T>
+int digits(T n) {
+  int ans = 0;
+  while (n != 0) {
+    ans++;
+    n = n/10;
+  }
+  return ans;
+}
+
+int frequency(string& str, char ch) {
+  int count = 0;
+  rep(i, 0, str.length())
+    if (str[i] == ch) count++;
+  return count;
+}
+
+int overlap(string& s1, string& s2) {
+  // 1 in case of single string
+  int start = 0;
+  int x = max(start, (int)(s1.length() - s2.length()));
+  for(;x<s1.length(); ++x) {
+    bool match = true;
+    for(int i = x, j = 0; i<s1.length() && j<s2.length(); ++i,++j) {
+      if(s1[i] != s2[j]) {
+        match = false;
+        break;
+      }
+    }
+    if (match)
+      return (int)s1.length() - x;
+  }
+  return 0;
+}
+
+int solve () {
+  return 0;
 }
 
 int main() {
-    freopen("/Users/knaresh/codechef/codechef/codechef/in.txt", "r", stdin);
-    freopen("/Users/knaresh/codechef/codechef/codechef/out.txt", "w", stdout);
-    int testCase = SS;
-    int testNo = 1;
-    int ans = 0;
-    while(testCase--) {
-        printf("Case #%d: %d\n", testNo++, ans);
-    }
-    return 0;
+  //freopen("/Users/knaresh/codejam/codejam/in.txt", "r", stdin);
+  //freopen("/Users/knaresh/codejam/codejam/out.txt", "w", stdout);
+  int tests = ss;
+  int no = 1;
+  while(tests--) {
+    printf("Case #%d: %d\n", no++, solve());
+  }
+  return 0;
 }
